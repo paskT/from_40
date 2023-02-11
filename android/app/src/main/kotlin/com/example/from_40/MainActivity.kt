015e8657c97b3d5f387f2ac247cd24d883af8b0f
paskT <pbhatta727@gmail.com>
2023-02-11 14:15:52 +0545

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/from_40/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.from_40
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/design_container.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main (){
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text('container'),
+      ),
+
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        color: Colors.green,
+        child: Center(
+          child :Container(
+          height: 150,
+          width: 150,
+           // color: Colors.cyan,
+            decoration: BoxDecoration(
+              color: Colors.blueGrey,
+             // borderRadius: BorderRadius.circular(11)//border lai change garne
+              borderRadius: BorderRadius.only(topLeft: Radius.circular(11),topRight:Radius.circular(11)
+            ),
+        ),
+        //foregroundDecoration: ,
+
+      ),
+    ),
+  )));
+
+
+}
